d71316204e24dd2646eb183807eb77bc0b1ba3f3
Shree Gillorkar <shreegillorkar02@gmail.com>
2022-05-24 22:57:25 +0530

    Add More Functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar title = "TextUtils" about = "About Us"/>
       <div className="container">
         <TextForm heading = "Enter the text to analyse below"/>
+        {/* <About></About> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,17 +23,17 @@ export default function TextForm(props) {
         setText(newText);
     }
 
-    const handleSpaceRemove = (event)=>{
-        //console.log("Remove Space Clicked");
-        let newText = text.replaceAll(" ","");
-        setText(newText);
+    const handleExtraSpace = ()=>{
+        //console.log("Remove Extra Space Clicked");
+        let newText = text.split(/[  ]+/);
+        setText(newText.join(" "));
     }
 
-    const handleOnlyNumbers = (event)=>{
-        //console.log("Get Numbers Clicked");
-        const regex = /[0-9//]/g;
-        const digits = text.match(regex);
-        setText(digits.join());
+    const handleCopyText = ()=>{
+        //console.log("Copy Text Clicked");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
     }
 
     const[text, setText] = useState('');
@@ -43,15 +43,13 @@ export default function TextForm(props) {
     <div className="container">
         <h1>{props.heading}</h1>
         <div className = "mb-3">
-           <textarea className = "form-control" value = {text} onChange = {handleOnChange} id = "mybox" row = "8"/>
+           <textarea className = "form-control" value = {text} onChange = {handleOnChange} id = "myBox" row = "8"/>
         </div>
         <button className="btn btn-primary mx-1"  onClick={handleUpClick} >Convert to UperCase</button>
         <button className="btn btn-primary mx-1" onClick={handleDownClick}>Convert to LowerCase</button> 
         <button className="btn btn-primary mx-1" onClick={handleClearText}>Clear Text</button> 
-        <button className="btn btn-primary mx-1" onClick={handleSpaceRemove}>Remove Space</button> 
-        <button className="btn btn-primary mx-1" onClick={handleOnlyNumbers}>Get Numbers</button> 
-
-
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Space</button> 
+        <button className="btn btn-primary mx-1" onClick={handleCopyText}>Copy Text</button>
 
     </div>
     <div className="container my-3">
